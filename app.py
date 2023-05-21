e3d2788f616c0c5a61492d759d9de496eec9451b
kraujinis <kraujinis@gmail.com>
2023-05-21 09:25:38 +0300

    not minor modifications

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import connect
 from main import MongoDb
-from pymongo.collection import Collection
 
+# from pymongo.collection import Collection
 
 
 # mongo config
-mongodb_host = 'localhost'
+mongodb_host = "127.0.0.1"
 mongodb_port = 27017
-database_name = 'books'
-collection_name = 'science_books'
+database_name = "books"
+collection_name = "science_books"
 
 mongo = MongoDb()
 # connection
@@ -16,12 +16,8 @@ db = connect.connect_to_mongodb(mongodb_host, mongodb_port, database_name)
 collection = db[collection_name]
 
 # --- insert
-# document = {
-#     "name": "Pilsutskis Marius",
-#     "age": 15,
-#     "email": "pilsustskis@gmail.com"
-# }
-# mongo.insert_create_document(collection=collection, document=document)
+document = {"name": "Pilsutskis Marius", "age": 15, "email": "pilsustskis@gmail.com"}
+mongo.insert_create_document(collection=collection, document=document)
 
 # Read
 query = {"age": 15}
@@ -32,6 +28,3 @@ print(results)
 # get collections
 collections = mongo.get_database_collection(db, collection_name)
 print(collections)
-
-
-
